990082cb647cc47fc38916a1b3472597c07c29a5
Anh Hoang <duhoang@davidson.edu>
2021-09-12 21:13:50 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,14 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+const matches = document.querySelectorAll("div");
+const number = matches.length;
+console.log(number);
+
+
+document.addEventListener('mousemove', (event) => {
+  var x = event.clientX;
+  var y = event.clientY;
+  var coordinates = "X: " + x + " Y: " + y;
+  document.getElementById("display").innerHTML = coordinates;
+});
